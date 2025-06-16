5a1fbed9f58afcdc44645eb7ebf6beef32993054
Sunil Prajapati <sunilp832@gmail.com>
2025-06-16 11:51:17 +0530

    Add files via upload

---
+++ hash_table.py
@@ -0,0 +1,33 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+
+    def __setitem__(self, key, value):
+        h = self.get_hash(key)
+        self.arr[h] = value
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+t = HashTable()
+t['march 6'] = 130
+t['march 1'] = 20
+t['march 17'] = 27
+
+print(t['march 6'])
+print(t.arr)
+
+del t['march 1']
+print(t['march 1'])
