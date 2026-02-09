c36afe4b8bbf3e4507541b6cf795c045ad9ecc3b
Sunil Prajapati <sunilp832@gmail.com>
2026-02-09 12:19:27 +0530

    Add files via upload

---
+++ SQL_Practices/SQLZeroToHeroSeries/Question 95 .1.sql
@@ -0,0 +1,15 @@
+-- Task 95 Find the running total of sales for each month.
+WITH CTE_Monthly_Summary AS (
+	SELECT 
+	DATETRUNC(month, OrderDate) OrderMonth,
+	SUM(Sales) TotalSales,
+	COUNT(OrderID) TotalOrders,
+	SUM(Quantity) TotalQuantities
+	FROM Sales.Orders
+	GROUP BY DATETRUNC(month, OrderDate)
+)
+SELECT 
+OrderMonth,
+TotalSales,
+SUM(TotalSales) OVER (ORDER BY OrderMonth) AS RunningTotal
+FROM CTE_Monthly_Summary
